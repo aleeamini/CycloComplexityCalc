5dcc075ac9a7df444c7e200225bbe0f9f5dff607
AleeAmini <32514487+aleeamini@users.noreply.github.com>
2020-08-01 00:19:34 +0430

    Add files via upload

---
+++ complexity.py
@@ -0,0 +1,331 @@
+from __future__ import print_function
+from collections import defaultdict
+import idaapi
+import idautils
+import idc
+from sets import Set
+
+class Graph: 
+
+    def __init__(self,vertices): 
+        #No. of vertices 
+        self.V= vertices  
+        self.looplist=list() 
+        # default dictionary to store graph 
+        self.graph = defaultdict(list)  
+          
+        self.Time = 0
+
+    # function to add an edge to graph 
+    def addEdge(self,v,u): 
+        self.graph[v].append(u) 
+
+    def printgrapph(self):
+        for keys,values in self.graph.iteritems():
+            for value in values:
+                print(keys," has relation with ",value)
+            
+    def SCCUtil(self,u, low, disc, stackMember, st): 
+        loops=list()
+        # Initialize discovery time and low value 
+        disc[u] = self.Time 
+        low[u] = self.Time 
+        self.Time += 1
+        stackMember[u] = True
+        st.append(u) 
+        for v in self.graph[u]: 
+            if disc[v] == -1 : 
+                self.SCCUtil(v, low, disc, stackMember, st) 
+                low[u] = min(low[u], low[v])             
+            elif stackMember[v] == True:  
+                low[u] = min(low[u], disc[v]) 
+        w = -1 #To store stack extracted vertices 
+        if low[u] == disc[u]: 
+            while w != u: 
+                w = st.pop() 
+                #print (w,end=',')
+                stackMember[w] = False 
+                loops.append(w);
+            #print("",end='\n')
+            if(len(loops)>1):
+                self.looplist.append(loops)
+                
+        return self.looplist
+
+    def SCC(self):
+        disc = [-1] * (self.V) 
+        low = [-1] * (self.V) 
+        stackMember = [False] * (self.V) 
+        st =list()
+        for i in range(self.V): 
+            if disc[i] == -1:
+                self.SCCUtil(i, low, disc, stackMember, st)
+        return self.looplist
+
+            
+
+#printFuncs(userfunc_dict)
+class CyclomaticComplexityChoose(Choose2):
+    def __init__(self, title):
+        Choose2.__init__(self, title, [ 
+                ["Address", 8 | Choose2.CHCOL_HEX], 
+                ["Name", 30 | Choose2.CHCOL_PLAIN],
+                ["BasicBlocks", 6 | Choose2.CHCOL_DEC],
+                ["TarjanLoops", 6 |Choose2.CHCOL_DEC],
+                ["Loops", 6 |Choose2.CHCOL_DEC],
+                ["SwitchCases", 6 | Choose2.CHCOL_DEC],
+                ["MaxPreds", 6 | Choose2.CHCOL_DEC],
+                ["Pointers", 6 | Choose2.CHCOL_DEC],
+                ["Cyclomatic_Complexity", 6 | Choose2.CHCOL_DEC],
+                ["Library func", 6 | Choose2.CHCOL_PLAIN] ])
+
+        self.title	= title
+        self.colors = (0x0000ff,0x0074ff, 0x00e8ff, 0x00ff00)
+        self.items	= []
+        self.icon	 = 41
+        self.PopulateItems()
+
+    def OnClose(self):
+        return True
+
+    def OnSelectLine(self, n):
+        item = self.items[int(n)]
+        jumpto(int(item[0], 16))
+
+    def OnGetLine(self, index):
+        return self.items[index]
+
+    def OnGetSize(self):
+        return len(self.items)
+
+    def OnDeleteLine(self, n):
+        del self.items[n]
+        return n
+
+    def OnGetLineAttr(self,n):
+        red=0x0000ff
+        green=0x00ff00
+        maxbbs=int(self.items[0][2])
+        bbs = int(self.items[n][2])
+        mid=maxbbs/2
+        if(n==0):
+            color=red
+        elif(bbs>mid):
+            tmpgreen=(((bbs*255 )/mid))
+            if(tmpgreen>255):
+                tmpgreen=255-(tmpgreen-255)
+            color=red|(tmpgreen<<8)
+            #print("%06x" %tmpgreen)
+        elif(bbs<mid):
+            tmpred=(bbs*255)/mid
+            color=green|(tmpred)
+            #print("%06x" %tmpred)
+        
+        return [color, 0]
+
+    def OnRefresh(self, n):
+        return n
+
+    def OnCommand(self, n, cmd_id):
+        if cmd_id == self.cmd_exc_lib_funcs:
+            self.exclude_lib_funcs()
+        return n
+
+    def exclude_lib_funcs(self):
+        if not len(self.items):
+            return False 
+        self.items = [i for i in self.items if i[3] != 'True']
+        return True
+
+    def show(self):
+        n_functions = len(list(Functions()))
+        if n_functions > 0:			
+            b = self.Show()
+            if b == 0:
+                #self.cmd_exc_lib_funcs = self.AddCommand("Exclude library functions")
+                return True
+        else:
+            warning("IDA has not identified functions.")
+            return False
+
+    def PopulateItems(self):
+        userfuncs=getUserFuncs()
+        
+        loopsdict={}
+        tarjansloopsdict={}
+        bbsdict={}
+        switchdict={}
+        predsdict={}
+        cyclodict={}
+        pointerdict={}
+        
+        for func in userfuncs:
+            loops=loopsInFunc(func)
+            loopsdict.update({func:len(loops)})
+            bbs=bbDetection(func)
+            bbsdict.update({func:bbs.size})
+            switchs=switchDetection(func)
+            switchdict.update({func:switchs})
+            preds=getBlockPredecessor(func)
+            predsdict.update({func:preds})
+            pointerdict.update({func:pointerCounter(func)})
+            tarjanloops=tarjansLoops(func)
+            tarjansloopsdict.update({func:tarjanloops})
+            cyclo=cyclomatic_complexity(func)
+            cyclodict.update({func:cyclo})
+            self.items.append(["%08x" % func,"%s" %userfuncs[func], "%d" % int(bbsdict[func]),"%d" % len(tarjansloopsdict[func]),"%d" % loopsdict[func],"%d" %len(switchdict[func]),
+                "%d" %len(predsdict[func]),"%d" %pointerdict[func],"%d" %cyclodict[func],"%s" % ((GetFunctionFlags(func) & FUNC_LIB) != 0)])
+            
+            self.items=sorted(self.items, key=lambda student: int(student[2]),reverse=True) 
+            
+            
+    def getFuncs():
+        ea = SegByBase(SegByName(".text"))
+        allfuncs_dict={}
+        for functionAddr in Functions(ea):
+            allfuncs_dict.update({functionAddr:GetFunctionName(functionAddr)})
+        return allfuncs_dict
+        
+    def getUserFuncs():
+        userfunc_dict={}
+        allfuncs=getFuncs()
+        for func in allfuncs:
+            flags=idc.GetFunctionAttr(func,idc.FUNCATTR_FLAGS)
+            if not(flags & FUNC_LIB) and not(flags & FUNC_THUNK):# exclude lib functions 
+                userfunc_dict.update({func:allfuncs[func]})
+        return userfunc_dict
+
+    def loopsInFunc(funcea):
+        loops = []
+        func_end = FindFuncEnd(funcea)
+        for item in FuncItems(funcea):
+            for xref in XrefsTo(item, 0):
+                if xref.type not in [1,21]:
+                    if funcea <= xref.to <= xref.frm <= func_end:
+                        if GetMnem(xref.frm) not in ['call', 'retn']:
+                            loops.append((hex(xref.frm), hex(xref.to)))
+        return loops
+
+    def bbDetection(funcea):
+        f=idaapi.get_func(funcea)
+        bblist=[]
+        bblist=idaapi.FlowChart(f)
+        return bblist
+
+    def getBlockPredecessor(funcea):
+        max=-1
+        blockaddr=0
+        i=0
+        func=idaapi.get_func(funcea);
+        flowchart=idaapi.FlowChart(func, None, 0x4)
+        for block in flowchart:
+            i=0
+            for s in block.preds():
+                i=i+1
+            if(i > max):
+                max=i
+                blockaddr=block.startEA
+
+        return [blockaddr,max]
+
+    def switchDetection(funcea):
+        switchDict={}
+        funcEndAddr=idc.GetFunctionAttr(funcea,idc.FUNCATTR_END)
+        for head_ea in Heads(funcea, funcEndAddr):
+            if idc.isCode(idc.GetFlags(head_ea)):
+                switch_info = idaapi.get_switch_info(head_ea)
+                if (switch_info and switch_info.jumps != 0):
+                    loc = switch_info.jumps
+                    element_num = switch_info.get_jtable_size()
+                    switchDict.update({loc:element_num})
+        return switchDict
+
+    def pointerCounter2(funcea):
+        switchDict={}
+        c=0
+        funcEndAddr=idc.GetFunctionAttr(funcea,idc.FUNCATTR_END)
+        heads=Heads(funcea, funcEndAddr)
+        for head_ea in heads:
+            if idc.isOff0(idc.GetFlags(head_ea)):
+                c=c+1
+                print (hex(head_ea))    
+        #return switchDict
+
+    def pointerCounter(funcea):### this function has been written by myself
+        switchDict={}
+        c=0
+        funcEndAddr=idc.GetFunctionAttr(funcea,idc.FUNCATTR_END)
+        heads=Heads(funcea, funcEndAddr)
+        for head_ea in heads:
+            if idc.isCode(idc.GetFlags(head_ea)):
+                if (idc.print_insn_mnem(head_ea) == "lea"):
+                    if (idc.get_operand_type(head_ea, 1) == idaapi.o_displ):
+                        nexthead=idc.next_head(head_ea)
+                        if (idc.print_insn_mnem(nexthead) == "mov"):
+                            if (idc.get_operand_type(nexthead, 0) == idaapi.o_displ):
+                                c=c+1
+                                #print (hex(head_ea))
+        return c
+
+        
+    def tarjansLoops(fva):
+        function = idaapi.get_func(fva)
+        flowchart = idaapi.FlowChart(function)
+        g=Graph(flowchart.size)
+        for bb in flowchart:
+            for succ in bb.succs():
+                g.addEdge(bb.id,succ.id)
+        return g.SCC()  
+
+    def cyclomatic_complexity(function_ea):
+        
+        f_start = function_ea
+        f_end = idc.FindFuncEnd(function_ea)
+        edges = Set()
+        boundaries = Set((f_start,))
+
+        for head in Heads(f_start, f_end):
+            if isCode(idc.GetFlags(head)):
+                refs = CodeRefsFrom(head, 0)
+                refs = Set(filter(lambda x: x>=f_start and x<=f_end, refs))
+                
+                if refs:
+                    next_head = NextHead(head, f_end)
+                    if isFlow(GetFlags(next_head)):
+                        refs.add(next_head)
+                    boundaries.union_update(refs)
+
+                    for r in refs:
+                        if isFlow(GetFlags(r)):
+                            edges.add((PrevHead(r, f_start), r))
+                        edges.add((head, r))
+
+        return len(edges) - len(boundaries) + 2    
+
+
+def show_choose():
+    choose = CyclomaticComplexityChoose("Complexity")
+    choose.show()
+
+class CyclomaticComplexity_t(plugin_t):
+    flags = PLUGIN_UNL
+    comment = "Complexity"
+    help = ""
+    wanted_name = "Complexity"
+    wanted_hotkey = ""
+
+    def init(self):
+        self.icon_id = 0
+        return PLUGIN_OK
+
+    def run(self, arg=0):
+        show_choose()
+
+    def term(self):
+        pass
+
+def PLUGIN_ENTRY():
+    return CyclomaticComplexity_t()
+
+if __name__ == '__main__':
+    show_choose()
